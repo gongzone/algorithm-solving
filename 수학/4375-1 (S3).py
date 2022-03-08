2a8583678aeb74edad3760e4dadf777cced15bb9
dnjsqhwo <dnjsqhwo@gmail.com>
2022-03-09 00:20:45 +0900

    공부 업데이트

--- 수학/4375-1 (S3).py
+++ 수학/4375-1 (S3).py
@@ -1,20 +1,16 @@
 import sys
 
-getInput = sys.stdin.readline
+for line in sys.stdin:
+  num = int(line)
+  comparing = 1
+  count = 1
 
-while True:
-  try:
-    num = int(getInput().rstrip())
-    comparing = 1
-    count = 1
-
-    while True:
-      if comparing % num == 0:
-        print(len(str(num)))
-        break
-      else:
-        comparing += 10 ** count
-
-      count += 1
-  except:
+  if num == 1:
+    print(1)
     break
+
+  while comparing != 0:
+    comparing = (comparing * 10 + 1) % num
+    count += 1
+  
+  print(count)
